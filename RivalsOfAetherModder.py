ec22967678f5a438965d368ede6d6ea4eff3999e
jam1-garner <jam1.mcleod@gmail.com>
2016-02-24 14:05:34 -0500

    Added the ability to quickly update offsets.txt

--- RivalsOfAetherModder.py
+++ RivalsOfAetherModder.py
@@ -9,11 +9,66 @@ def offsetsToList():
     offsets.close()
     return offsetList
 
-offsets = offsetsToList()
+########PNG FORMAT###############
+#Hex - 89 50 4E 47
+#Dec - 137 80 78 71
+
+#through
+
+#Hex - 49 45 4E 44 AE 42 60 82
+#Dec - 73 69 78 68 174 66 96 130
+#################################
+
+def newOffsetsToList():
+    offsetList = []
+    lastEight = []
+    currentTuple = [0,0]
+    currentOffset = -1
+    currentValue = ' '
+    rivals = open('RivalsofAether.exe','rb')
+    while currentValue != '':
+        currentOffset += 1
+        currentValue = rivals.read(1)
+        if currentValue != '':
+            lastEight.append(ord(currentValue))
+        #print 'Offset - '+str(currentOffset)+' | Last Eight - '+str(lastEight)
+        if len(lastEight) > 8:
+            del lastEight[0]
+        if lastEight[len(lastEight)-4:] == [137,80,78,71]:
+            currentTuple[0] = currentOffset - 3
+            print 'Start - '+str(currentTuple[0])
+        if lastEight == [73,69,78,68,174,66,96,130]:
+            currentTuple[1] = currentOffset + 1
+            print 'End - '+str(currentTuple[1])
+            offsetList.append(tuple(currentTuple))
+        
+    rivals.close()
+    print offsetList
+    return offsetList
+
+def saveOffsetsFromList(offsets):
+    offsetFile = open('offsets.txt','w')
+    for start,end in offsets:
+        offsetFile.write(str(start)+'-'+str(end)+'\n')
+    offsetFile.close()
+
 if os.path.realpath(__file__)[len(os.path.realpath(__file__))-23:] != 'RivalsOfAetherModder.py':
     print 'SCRIPT SHOULD BE NAMED RivalsOfAetherModder.py, REENAMING IT MAY CAUSE ISSUES'
 path = os.path.realpath(__file__)[:len(os.path.realpath(__file__))-23]
-ripType = raw_input('\'rip\' or \'replace\'? ')
+ripType = 'help'
+while ripType == 'help':
+    ripType = raw_input('\'rip\', \'replace\', \'update\' or \'help\'? ')
+    if ripType == 'help':
+        print '* rip - rip the sprites from the exe to your sprites folder'
+        print '* replace - take the sprites from your sprites folder and put them in your exe'
+        print '* update - update your offsets.txt, use whenever a new patch releases'
+        
+
+if ripType == 'update':
+    print 'Updating sprite offsets this may take a while...'
+    saveOffsetsFromList(newOffsetsToList())
+    
+offsets = offsetsToList()
 if ripType == 'rip':
     print 'Beginning sprite rip...'
     rivalsEXE = open('RivalsofAether.exe','rb')
@@ -41,6 +96,6 @@ elif ripType == 'replace':
         rivalsEXE.write(f.read((start-end)-1))
         f.close()
     rivalsEXE.close()
-else:
+elif ripType != 'update':
     print 'Not a specified type.'
 
